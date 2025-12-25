89bdd619de09ae010aeaaf641662adb47380993f
LJH <LJH@DESKTOP-75RJLKC>
2025-12-25 19:17:13 +0800

    添加项目文件。

---
+++ Battlenet/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using BattlenetResources.Controls;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Battlenet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 3秒钟 后显示登录窗口
+
+            InitialzeApp();
+        }
+
+        private async void InitialzeApp()
+        {
+            await Task.Delay(1);
+            new Thread(() => {
+                Thread.Sleep(3000); 
+                this.Dispatcher.Invoke(() =>
+                {
+                    win.Height = 599;
+                    ContentShow.Content = new Register();
+                });
+            }).Start();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            // 关闭当前窗口
+            this.Close();
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            // 将窗口状态设置为最小化
+            this.WindowState = WindowState.Minimized;
+        }
+
+    }
+}
---
+++ BattlenetResources/Controls/Register.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BattlenetResources.Controls
+{
+    /// <summary>
+    /// Register.xaml 的交互逻辑
+    /// </summary>
+    public partial class Register : UserControl
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            // 获取协议网址
+            string url = "https://legal.battlenet.com.cn/legal/2e90163d-612f-41a5-addd-8a837ac43d02";
+
+            // 调用系统浏览器打开
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true // 必须设置为 true 才能在 .NET Core/5+ 中生效
+            });
+        }
+    }
+}
